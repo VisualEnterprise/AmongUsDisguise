4e45a76113ce5f62a4e5877be7c0bfa238d8dbf4
tsieben <tim@tsieben.com>
2020-12-29 15:23:44 -0500

    Initial codebase

---
+++ AmongUsDisguise/Disguise.cs
@@ -0,0 +1,38 @@
+﻿using Impostor.Api.Events.Managers;
+using Impostor.Api.Plugins;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace cc.ts13.AmongUsDisguise {
+
+    [ImpostorPlugin(
+        package: "cc.ts13.AmongUsDisguise",
+        name: "AmongUsDisguise",
+        author: "Siebs",
+        version: "1.0.0")]
+    public class Disguise : PluginBase {
+
+        private readonly ILogger<Disguise> _logger;
+        private readonly IEventManager _eventManager;
+        private IDisposable _unregister;
+
+        public Disguise(ILogger<Disguise> logger, IEventManager eventManager) {
+            _logger = logger;
+            _eventManager = eventManager;
+        }
+
+        public override ValueTask EnableAsync() {
+            _logger.LogInformation("Detective is being enabled");
+            _unregister = _eventManager.RegisterListener(new GameEventListener(_logger));
+            return default;
+        }
+
+        public override ValueTask DisableAsync() {
+            _logger.LogInformation("Detective is being disabled");
+            _unregister.Dispose();
+            return default;
+        }
+
+    }
+}
---
+++ AmongUsDisguise/GameEventListener.cs
@@ -0,0 +1,59 @@
+﻿using Impostor.Api.Events;
+using Impostor.Api.Events.Meeting;
+using Impostor.Api.Events.Player;
+using Impostor.Api.Net;
+using Impostor.Api.Net.Inner.Objects;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cc.ts13.AmongUsDisguise.Handlers {
+    class GameEventListener : IEventListener {
+
+        private readonly ILogger<Disguise> _logger;
+        private Dictionary<IInnerPlayerControl, IInnerPlayerControl> _kills;
+
+        public GameEventListener(ILogger<Disguise> logger) {
+            _logger = logger;
+            _kills = new Dictionary<IInnerPlayerControl, IInnerPlayerControl>();
+        }
+
+        [EventListener]
+        public void OnGameStarted(IGameStartedEvent e) {
+            _logger.LogInformation($"Game is starting.");
+        }
+
+        [EventListener]
+        public void OnPlayerMurder(IPlayerMurderEvent e) {
+            _logger.LogInformation($"Player murdered");
+            _kills.Add(e.Victim, e.PlayerControl);
+            IInnerPlayerControl murderer = e.PlayerControl, victim = e.Victim;
+            string mPlayerName = murderer.PlayerInfo.PlayerName, 
+                vPlayerName = victim.PlayerInfo.PlayerName;
+            byte mColorId = murderer.PlayerInfo.ColorId, 
+                vColorId = victim.PlayerInfo.ColorId;
+            uint mHatId = murderer.PlayerInfo.HatId, 
+                vHatId = victim.PlayerInfo.HatId, 
+                mPetId = murderer.PlayerInfo.PetId, 
+                vPetId = victim.PlayerInfo.PetId, 
+                mSkinId = murderer.PlayerInfo.SkinId, 
+                vSkinId = victim.PlayerInfo.SkinId;
+        }
+
+        [EventListener]
+        public void OnGameEnd(IGameEndedEvent e) {
+            _logger.LogInformation($"Game has ended.");
+        }
+
+        [EventListener]
+        public void OnPlayerChat(IPlayerChatEvent e) {
+            _logger.LogInformation($"{e.PlayerControl.PlayerInfo.PlayerName} said {e.Message}");
+        }
+
+        [EventListener]
+        public void OnPlayerSpawned(IPlayerSpawnedEvent e) {
+        }
+    }
+}
